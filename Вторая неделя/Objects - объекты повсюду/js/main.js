f28926e0bb4af594b5d251b7197fb40e1d6fb18b
Maxim <ivanenkomax@rambler.ru>
2023-09-19 16:11:38 +0300

    +

---
+++ Вторая неделя/Objects - объекты повсюду/js/main.js
@@ -0,0 +1,24 @@
+/* *************************
+Объекты повсюду
+**************************** */
+
+var testFun = function () {
+    return "Test Function";
+};
+
+var newArray = [1, 2, 3, 4, 5];
+var obj = { name: "Yurij" };
+const today = new Date();
+
+// console.log(testFun);
+// console.dir(testFun);
+// testFun.today = new Date();
+
+// newArray.name = "My array";
+
+// var str = "new string";
+// str.length;
+// str.split(" ");
+
+// window.location
+// window.document.title
