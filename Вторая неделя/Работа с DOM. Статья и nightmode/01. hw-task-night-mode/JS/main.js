474027dc40f42b8d73cdac48ce16bb898fb07d61
Maxim <ivanenkomax@rambler.ru>
2023-09-27 13:59:30 +0300

    +

--- Вторая неделя/Работа с DOM. Статья и nightmode/01. hw-task-night-mode/JS/main.js
+++ Вторая неделя/Работа с DOM. Статья и nightmode/01. hw-task-night-mode/JS/main.js
@@ -1,24 +1,13 @@
 let nightModeButton = document.querySelector("#night-mode");
-let elements = document.querySelectorAll("h1, h2, a, p, article");
-let nightModeButtonActive = true;
+let bodeElem = document.querySelector("body");
 
 nightModeButton.addEventListener("click", function () {
 
-    // Выбираем все нужные элементы
-    elements.forEach(function (element) {
-        if (!element.classList.contains("night")) {
-            // Добавляем класс "night" к каждому элементу
-            element.classList.add("night");
-        } else {
-            element.classList.remove("night");
-        }
-    });
-
-    nightModeButtonActive = !nightModeButtonActive;
-
-    if (nightModeButton.textContent = !nightModeButtonActive) {
-        nightModeButton.textContent = "Выключить ночной режим";
-    } else {
+    if (bodeElem.classList.contains("night")) {
+        bodeElem.classList.remove("night");
         nightModeButton.textContent = "Включить ночной режим";
+    } else {
+        bodeElem.classList.add("night");
+        nightModeButton.textContent = "Выключить ночной режим";
     }
 });
