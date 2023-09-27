ca3b32354e68c460c88e3d9c6c8cd192d4e3579d
Maxim <ivanenkomax@rambler.ru>
2023-09-27 17:24:45 +0300

    +

--- Вторая неделя/Работа с DOM. Атрибуты. Выбор цвета для авто. Задание/Архив с стартовым проектом/HW-task-color-filter/js/main.js
+++ Вторая неделя/Работа с DOM. Атрибуты. Выбор цвета для авто. Задание/Архив с стартовым проектом/HW-task-color-filter/js/main.js
@@ -1,17 +1,55 @@
-/*
-Задание:
-Сделать рабочий фильтр цветов.
-При нажатии на один из цветов, круг с цветом - мы видим автомобиль, соответствующего цвета.
-А активный цвет выделяется, за счет дополнительного CSS класса ".colorItem--active".
+// Найти действующие элементы
 
-Работу сдать через CodePen.
+let imgHolderEl = document.querySelector("#imgHolder");
+let colorsItemsEl = document.querySelectorAll(".colorItem");
+let imgCarEl = imgHolderEl.querySelector("img");
 
-Другие изображения автомобиля:
-https://webcademy.ru/files/js2020/solaris/black.png
-https://webcademy.ru/files/js2020/solaris/blue.png
-https://webcademy.ru/files/js2020/solaris/graphite.png
-https://webcademy.ru/files/js2020/solaris/orange.png
-https://webcademy.ru/files/js2020/solaris/red.png
-https://webcademy.ru/files/js2020/solaris/white.png
-https://webcademy.ru/files/js2020/solaris/white-pure.png
-*/
+// Отслежка пользовательских событий
+colorsItemsEl.forEach(function (colorItemEl) {
+    colorItemEl.addEventListener("click", function (e) {
+
+        // Функции которые сработают на действие пользовательские действия
+
+        /* if (event.target.classList.contains('colorBlue')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/blue.png';
+           }
+   
+           if (event.target.classList.contains('colorWhite')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/white.png';
+           }
+   
+           if (event.target.classList.contains('colorRed')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/red.png';
+           }
+   
+           if (event.target.classList.contains('colorBlack')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/black.png';
+           }
+   
+           if (event.target.classList.contains('colorWhitePure')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/white-pure.png';
+           }
+   
+           if (event.target.classList.contains('colorOrange')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/orange.png';
+           }
+   
+           if (event.target.classList.contains('colorGraphite')) {
+               img.src = 'https://webcademy.ru/files/js2020/solaris/graphite.png';
+           } */
+
+
+        const fileName = e.target.dataset.file;
+        let newSrc = 'https://webcademy.ru/files/js2020/solaris/' + fileName;
+
+        imgCarEl.src = newSrc;
+
+        // Убираем активный класс
+        const colorItemActive = document.querySelector(".colorItem--active")
+        colorItemActive.classList.remove("colorItem--active");
+
+        // Делаем кнопку активной, добавляем класс colorItem--active
+        e.target.classList.add("colorItem--active")
+
+    });
+});
