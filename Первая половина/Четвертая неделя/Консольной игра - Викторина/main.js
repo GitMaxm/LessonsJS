f6e929a997dceec1e448e24358910842aa596dba
Ivan <ivanenkomax@rambler.ru>
2023-10-09 23:39:14 +0300

    +

--- Первая половина/Четвертая неделя/Консольной игра - Викторина/main.js
+++ Первая половина/Четвертая неделя/Консольной игра - Викторина/main.js
@@ -1,18 +1,23 @@
-let Question = function (numberQuestion, question, answer, trueAnswer) {
+let Question = function (numberQuestion, question, optionsAnswer, trueAnswer) {
     this.numberQuestion = numberQuestion;
     this.question = question;
-    this.answer = answer;
+    this.optionsAnswer = optionsAnswer;
     this.trueAnswer = trueAnswer;
-    this.checkAnswer = function (userAnswer) {
-        return userAnswer === this.trueAnswer;
-    }
+    // this.checkAnswer = function (userAnswer) {
+    //     if (userAnswer === this.trueAnswer) {
+    //         point++;
+    //         console.log("Ваши очки: " + point);
+    //     } else {
+    //         console.log("Вы ввели неправильный ответ");
+    //     }
+    // }
 }
 
 let questionOne = new Question(
     1,
     "Какие основные типы данных существуют в JavaScript?",
     ["String", "Number", "Boolean"],
-    1
+    0,
 );
 
 let questionTwo = new Question(
@@ -21,7 +26,7 @@ let questionTwo = new Question(
     ["Функция внутри функции",
         "Способ сохранения доступа к переменным из внешней функции",
         "Встроенный метод для замыкания дверей"],
-    2
+    1
 );
 
 let questionThree = new Question(
@@ -30,7 +35,7 @@ let questionThree = new Question(
     ["let и const позволяют создавать переменные с блочной областью видимости, в отличие от var",
         "var может быть переопределена, а let и const - нет",
         "const создает неизменяемую переменную, а let и var - нет"],
-    1
+    0
 );
 
 let questionFour = new Question(
@@ -39,8 +44,38 @@ let questionFour = new Question(
     ["block",
         "scope",
         "let"],
-    3
+    2
 );
 
+(function () {
+    let point = 0;
 
+    console.log(questionThree.question);
 
+    questionThree.optionsAnswer.forEach(function (item, index) {
+        console.log(`${index}) ${item}`);
+    });
+
+    let userAnswer = prompt("Введите номер верного ответа:");
+
+    if (userAnswer !== null) {
+
+        if (userAnswer === questionThree.trueAnswer) {
+
+            if (userAnswer >= 0 && userAnswer <= 2) {
+                point++;
+                console.log("Ваши очки: " + point);
+            } else {
+                alert("Число должно быть больше от 0 до 2!")
+            }
+
+        } else {
+            alert("Вы ввели неправильный ответ!")
+        }
+
+    } else {
+        console.log("Пользователь нажал Отмена. Операция отменена.");
+        return;
+    }
+
+})();
