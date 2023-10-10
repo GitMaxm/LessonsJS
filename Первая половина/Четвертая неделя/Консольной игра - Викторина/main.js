6634a3c1feebc08e147c767d993d363dea60fb94
Maxim <ivanenkomax@rambler.ru>
2023-10-10 19:09:22 +0300

    +

--- Первая половина/Четвертая неделя/Консольной игра - Викторина/main.js
+++ Первая половина/Четвертая неделя/Консольной игра - Викторина/main.js
@@ -1,27 +1,37 @@
-let Question = function (numberQuestion, question, optionsAnswer, trueAnswer) {
-    this.numberQuestion = numberQuestion;
+let Question = function (question, optionsAnswer, trueAnswer) {
     this.question = question;
     this.optionsAnswer = optionsAnswer;
     this.trueAnswer = trueAnswer;
-    // this.checkAnswer = function (userAnswer) {
-    //     if (userAnswer === this.trueAnswer) {
-    //         point++;
-    //         console.log("Ваши очки: " + point);
-    //     } else {
-    //         console.log("Вы ввели неправильный ответ");
-    //     }
-    // }
+}
+
+Question.prototype.displayQuestion = function () {
+    console.log(this.question);
+    for (let i = 0; i < this.optionsAnswer.length; i++) {
+        console.log(`${i}) ${this.optionsAnswer[i]}`);
+    }
+}
+
+Question.prototype.comparisonAnswer = function () {
+    if (userAnswer === this.trueAnswer) {
+
+        if (userAnswer >= 0 && userAnswer <= 2) {
+            // point++;
+            console.log("Ваши очки: " + point);
+        } else {
+            alert("Число должно быть больше от 0 до 2!")
+        }
+    } else {
+        alert("Вы ввели неправильный ответ!")
+    }
 }
 
 let questionOne = new Question(
-    1,
     "Какие основные типы данных существуют в JavaScript?",
     ["String", "Number", "Boolean"],
     0,
 );
 
 let questionTwo = new Question(
-    2,
     "Что такое замыкание (closure) в JavaScript?",
     ["Функция внутри функции",
         "Способ сохранения доступа к переменным из внешней функции",
@@ -30,7 +40,6 @@ let questionTwo = new Question(
 );
 
 let questionThree = new Question(
-    3,
     "Какие различия существуют между let, const и var в JavaScript?",
     ["let и const позволяют создавать переменные с блочной областью видимости, в отличие от var",
         "var может быть переопределена, а let и const - нет",
@@ -39,7 +48,6 @@ let questionThree = new Question(
 );
 
 let questionFour = new Question(
-    4,
     "Какое ключевое слово используется для объявления переменной с блочной областью видимости?",
     ["block",
         "scope",
@@ -47,35 +55,22 @@ let questionFour = new Question(
     2
 );
 
-(function () {
-    let point = 0;
+const arrayQuestion = [questionOne, questionTwo, questionThree, questionFour];
+const randomIndex = Math.floor(Math.random() * arrayQuestion.length);
 
-    console.log(questionThree.question);
+arrayQuestion[randomIndex].displayQuestion();
 
-    questionThree.optionsAnswer.forEach(function (item, index) {
-        console.log(`${index}) ${item}`);
-    });
 
-    let userAnswer = prompt("Введите номер верного ответа:");
+let userAnswer = prompt("Введите номер верного ответа:");
 
-    if (userAnswer !== null) {
+arrayQuestion[randomIndex].comparisonAnswer();
+// if (userAnswer !== null) {
 
-        if (userAnswer === questionThree.trueAnswer) {
 
-            if (userAnswer >= 0 && userAnswer <= 2) {
-                point++;
-                console.log("Ваши очки: " + point);
-            } else {
-                alert("Число должно быть больше от 0 до 2!")
-            }
 
-        } else {
-            alert("Вы ввели неправильный ответ!")
-        }
+    // } else {
+    //     console.log("Пользователь нажал Отмена. Операция отменена.");
+    //     return;
+    // }
 
-    } else {
-        console.log("Пользователь нажал Отмена. Операция отменена.");
-        return;
-    }
-
-})();
+// })();
